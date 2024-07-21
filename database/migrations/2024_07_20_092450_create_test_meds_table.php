a3202d05f3004d8171296b8dc93438cc8e76324b
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-07-21 22:49:46 +0200

    dump fixings

--- database/migrations/2024_07_20_092450_create_test_meds_table.php
+++ database/migrations/2024_07_20_092450_create_test_meds_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('testmeds', function (Blueprint $table) {
+        Schema::create('test_meds', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('surname');
