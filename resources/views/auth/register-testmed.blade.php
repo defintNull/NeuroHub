701bd1c62f1351eb891e53c6f34c6d6c07950385
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-07-22 15:21:14 +0200

    registration navigation

--- resources/views/auth/register-testmed.blade.php
+++ resources/views/auth/register-testmed.blade.php
@@ -1,40 +1,49 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('testmed.register') }}">
-        @csrf
+<x-app-layout>
+    <div class="font-sans text-gray-900 antialiased">
+        <div class="mt-12 flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div>
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </div>
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <form method="POST" action="{{ route('testmed.register') }}">
+                    @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="given-name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <!-- Name -->
+                    <div>
+                        <x-input-label for="name" :value="__('Name')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="given-name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Surname -->
+                    <div>
+                        <x-input-label for="surname" :value="__('Surname')" />
+                        <x-text-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required autofocus autocomplete="family-name" />
+                        <x-input-error :messages="$errors->get('surname')" class="mt-2" />
+                    </div>
+
+                    <!-- Telephone -->
+                    <div>
+                        <x-input-label for="telephone" :value="__('Telephone')" />
+                        <x-text-input id="telephone" class="block mt-1 w-full" type="text" name="telephone" :value="old('telephone')" required autofocus autocomplete="tel" />
+                        <x-input-error :messages="$errors->get('telephone')" class="mt-2" />
+                    </div>
+
+                    <!-- Birthdate -->
+                    <div>
+                        <x-input-label for="birthdate" :value="__('Birthdate')" />
+                        <x-text-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate" :value="old('birthdate')" required autofocus autocomplete="bday" />
+                        <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-6">
+                        <x-primary-button class="ms-4">
+                            {{ __('Register') }}
+                        </x-primary-button>
+                    </div>
+
+                </form>
+            </div>
         </div>
-
-        <!-- Surname -->
-        <div>
-            <x-input-label for="surname" :value="__('Surname')" />
-            <x-text-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required autofocus autocomplete="family-name" />
-            <x-input-error :messages="$errors->get('surname')" class="mt-2" />
-        </div>
-
-        <!-- Telephone -->
-        <div>
-            <x-input-label for="telephone" :value="__('Telephone')" />
-            <x-text-input id="telephone" class="block mt-1 w-full" type="text" name="telephone" :value="old('telephone')" required autofocus autocomplete="tel" />
-            <x-input-error :messages="$errors->get('telephone')" class="mt-2" />
-        </div>
-
-        <!-- Birthdate -->
-        <div>
-            <x-input-label for="birthdate" :value="__('Birthdate')" />
-            <x-text-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate" :value="old('birthdate')" required autofocus autocomplete="bday" />
-            <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-6">
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-
-    </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
