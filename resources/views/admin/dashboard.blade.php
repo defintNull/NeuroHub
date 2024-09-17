e0114fd3046f36cf7c0167ea1cbf3dde932b6a27
andianno <andrea.iannotti@student.univaq.it>
2024-09-17 12:25:59 +0200

    admin graph

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,15 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-{{--                 <div class="p-6 text-gray-900">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
+                {{--                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div> --}}
 
                 <div class="m-4">
-                    <form method="GET" action="">
+                    <p class="text-xl font-semibold mb-2"> {{ __("Test Report") }} </p>
                         <label for="testname"> {{ __('Select a test:') }} </label>
-                        <select name="test" id="testname" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                        <select name="test" id="testname"
+                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                             <option value="all">All...</option>
                             @foreach ($tests as $test)
                                 <option value="{{ $test->id }}" @if (isset($sel) && $sel == $test->id) selected @endif>
@@ -26,24 +27,32 @@
                         <input id="date1" type="date" name="datemin" autocomplete="day"
                             max="{{ now()->format('Y-m-d') }}"
                             @if (isset($datemin)) value="{{ $datemin }}" @endif
-                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"/>
+                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" />
 
                         <label for="date2"> {{ __('To:') }} </label>
                         <input id="date2" type="date" name="datemax" autocomplete="day"
                             max="{{ now()->format('Y-m-d') }}"
                             @if (isset($datemax)) value="{{ $datemax }}" @endif
-                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"/>
+                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" />
+                        <label for="graph" hidden id="graphlabel" class=" ml-4"> {{ __('Select a type:') }} </label>
+                        <select name="graph" id="graph" hidden
+                            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                            <option value="line">{{ __('# of subministrations') }}</option>
+                            <option value="doughnut">{{ __('score distribution') }}</option>
+                        </select>
 
-                            <p class="p-4 text-center text-sm text-red-600"></p>
+                        <button type="submit" id="submitbtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
 
-                    </form>
+                        <p class="p-4 text-center text-sm text-red-600"></p>
                 </div>
 
-                <div class="p-6">
-                    <canvas id="myChart"></canvas>
+                <div class="flex flex-col items-center">
+                    <div class="p-6" id="canvascontainer">
+                        <canvas id="myChart"></canvas>
+                    </div>
                 </div>
 
-{{--                 <script>
+                {{--                 <script>
                     var d = {{ Js::from($data) }};
                 </script> --}}
             </div>
