04fbf1b9ea9d527b6937210f27a8b34722964462
andianno <andrea.iannotti@student.univaq.it>
2024-09-17 19:15:16 +0200

    dashboard

--- resources/views/testmed/dashboard.blade.php
+++ resources/views/testmed/dashboard.blade.php
@@ -8,8 +8,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="mt-10 p-10 text-gray-900 text-center text-4xl">
+                    {{ __("Welcome back ". Auth::user()->userable->name."!") }}
+                </div>
+
+                <div class="m-4 items-center">
+                    <div class="flex lg:justify-center lg:col-start-2">
+                        <img src="/logo_neurohub.svg" height="300" width="300" alt="Logo NeuroHub"/>
+                    </div>
                 </div>
             </div>
         </div>
