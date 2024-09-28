ea510dead4a4b8bdcc254f4a12858b5a7dc5e5ed
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-09-28 14:19:14 +0200

    fixing guest middleware

--- app/Http/Middleware/GuestMiddleware.php
+++ app/Http/Middleware/GuestMiddleware.php
@@ -19,7 +19,7 @@ class GuestMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if(Auth::check()) {
-            $usertype = get_class($request->user()->userable);
+            $usertype = $request->user()->userable_type;
             if($usertype == Admin::class) {
                 return redirect(route('admin.dashboard', absolute:false));
             } elseif($usertype == TestMed::class) {
@@ -29,11 +29,6 @@ class GuestMiddleware
             }
         } else {
             return $next($request);
-        }
-        if($request->session()->get('activevisit') !== null) {
-            return $next($request);
-        } else {
-            return redirect(route('med.dashboard', absolute:false));
         }
     }
 }
